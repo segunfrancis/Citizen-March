612fb4d2d71725db68869f41b1ec6a42d8372335
SegunFrancis <francis4dblues@gmail.com>
2020-06-30 06:28:40 +0100

    Display user profile on nav drawer header

--- app/src/main/java/com/project/segunfrancis/citizenmarch/ui/main/MainActivity.java
+++ app/src/main/java/com/project/segunfrancis/citizenmarch/ui/main/MainActivity.java
@@ -3,11 +3,15 @@ package com.project.segunfrancis.citizenmarch.ui.main;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 import com.project.segunfrancis.citizenmarch.R;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,10 +19,12 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private MainViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +32,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_create_march, R.id.nav_settings, R.id.nav_sign_out)
                 .setOpenableLayout(drawer)
@@ -59,8 +64,16 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-    }
 
+        // Inflate navigation drawer header view
+        View header = navigationView.getHeaderView(0);
+        CircleImageView imageView = header.findViewById(R.id.profile_imageView);
+        TextView name = header.findViewById(R.id.profile_name);
+        TextView email = header.findViewById(R.id.profile_email);
+        mViewModel.name().observe(this, name::setText);
+        mViewModel.email().observe(this, email::setText);
+        mViewModel.profilePhotoUrl().observe(this, s -> Glide.with(getApplicationContext()).load(s).into(imageView));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -74,5 +87,9 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    private void displaySnackBar(String message) {
+        Snackbar.make(findViewById(R.id.drawer_layout), message, Snackbar.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/project/segunfrancis/citizenmarch/ui/main/MainViewModel.java
+++ app/src/main/java/com/project/segunfrancis/citizenmarch/ui/main/MainViewModel.java
@@ -1,9 +1,45 @@
 package com.project.segunfrancis.citizenmarch.ui.main;
 
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 /**
  * Created by SegunFrancis
  */
 public class MainViewModel extends ViewModel {
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private MutableLiveData<String> _name = new MutableLiveData<>();
+    private MutableLiveData<String> _email = new MutableLiveData<>();
+    private MutableLiveData<String> _profilePhotoUrl = new MutableLiveData<>();
+
+    public LiveData<String> name() {
+        return _name;
+    }
+
+    public LiveData<String> email() {
+        return _email;
+    }
+
+    public LiveData<String> profilePhotoUrl() {
+        return _profilePhotoUrl;
+    }
+
+    public MainViewModel() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        _name.setValue(user.getDisplayName());
+        _email.setValue(user.getEmail());
+        _profilePhotoUrl.setValue(user.getPhotoUrl().toString());
+        Log.d("MainViewModel", "Name: " + user.getDisplayName());
+        Log.d("MainViewModel", "Email: " + user.getEmail());
+        Log.d("MainViewModel", "PhotoUrl: " + user.getPhotoUrl());
+    }
 }
