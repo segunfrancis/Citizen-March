b23f01f9ddec89263696ab12a4dc81ff3f21aa30
SegunFrancis <francis4dblues@gmail.com>
2020-07-04 12:43:02 +0100

    load profile image using async task

--- app/src/main/java/com/project/segunfrancis/citizenmarch/ui/settings/BindingAdapters.java
+++ app/src/main/java/com/project/segunfrancis/citizenmarch/ui/settings/BindingAdapters.java
@@ -1,7 +1,17 @@
 package com.project.segunfrancis.citizenmarch.ui.settings;
 
-import com.bumptech.glide.Glide;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+
 import com.project.segunfrancis.citizenmarch.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import androidx.databinding.BindingAdapter;
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -13,10 +23,36 @@ public class BindingAdapters {
 
     @BindingAdapter("loadImage")
     public static void loadProfileImage(CircleImageView imageView, String url) {
-        Glide.with(imageView.getContext())
-                .load(url)
-                .placeholder(R.drawable.loading_animation)
-                .error(R.drawable.ic_broken_image_24dp)
-                .into(imageView);
+        imageView.setTag(url);
+        new DownloadImageTask().execute(imageView);
+    }
+
+    public static class DownloadImageTask extends AsyncTask<CircleImageView, Void, Bitmap> {
+        CircleImageView imageView = null;
+
+        @Override
+        protected Bitmap doInBackground(CircleImageView... imageViews) {
+            this.imageView = imageViews[0];
+            return downloadImageFromUrl((String) imageView.getTag());
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            imageView.setImageBitmap(bitmap);
+        }
+
+        private Bitmap downloadImageFromUrl(String src) {
+            try {
+                URL url = new URL(src);
+                HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                return BitmapFactory.decodeStream(inputStream);
+            } catch (IOException e) {
+                Log.e("DownloadImageTask", e.getLocalizedMessage());
+                return null;
+            }
+        }
     }
 }
